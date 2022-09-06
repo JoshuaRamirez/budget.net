cfd085e10ebb26a4fa47a03b0776ec1a0dc2868b
Joshua Ramirez <joshua.ramiez@me.com>
2022-09-05 19:21:08 -1000

    Updated convention for creates

--- Budget.Application/Services/Creates/CreateAccountService.cs
+++ Budget.Application/Services/Creates/CreateAccountService.cs
@@ -1,11 +1,7 @@
-﻿using Budget.Application.Events.Core;
-using Budget.Application.Events.Created;
+﻿using Budget.Application.Events.Created;
 using Budget.Application.Events.Requested.Creation;
 using Budget.Application.Projections;
 using Budget.Application.Services.Core;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Budget.Application.Services.Creates
 {
@@ -17,15 +13,15 @@ namespace Budget.Application.Services.Creates
         }
         public override void Serve(AccountRequested @event)
         {
-            // Create AccountProjection
-            var accountProjection = new Account();
-            accountProjection.AccountName = @event.AccountName;
-            accountProjection.UserId = @event.UserId;
-            Account.Projections.Add(accountProjection);
-            // Publish AccountCreatedEvent
-            var accountCreated = new AccountCreated();
-            accountCreated.AccountId = accountProjection.Id;
-            AccountCreated.Publish(accountCreated);
+            // Create Projection
+            var projection = new Account();
+            projection.AccountName = @event.AccountName;
+            projection.UserId = @event.UserId;
+            Account.Projections.Add(projection);
+            // Publish Created Event
+            var createdEvent = new AccountCreated();
+            createdEvent.AccountId = projection.Id;
+            AccountCreated.Publish(createdEvent);
         }
     }
 }
