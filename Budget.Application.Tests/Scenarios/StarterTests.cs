f8eafea49d911d846e8ccfc434021ddebb0b88d9
Joshua Ramirez <joshua.ramiez@me.com>
2022-09-05 19:22:04 -1000

    Moved & Updated Starter Tests

--- Budget.Application.Tests/Scenarios/StarterTests.cs
+++ Budget.Application.Tests/Scenarios/StarterTests.cs
@@ -1,11 +1,12 @@
 using Budget.Application.Events.Requested.Creation;
-using Budget.Application.Projection;
+using Budget.Application.Projections;
 using Budget.Application.Services.Creates;
 using Budget.Application.Services.Links;
 using System;
+using System.Linq;
 using Xunit;
 
-namespace Budget.Application.Tests
+namespace Budget.Application.Tests.Scenarios
 {
     public class StarterTests
     {
@@ -16,18 +17,18 @@ namespace Budget.Application.Tests
             var createUserService = new CreateUserService();
             var createAccountService = new CreateAccountService();
             var linkAccountToUserService = new AccountToUser();
-            
+
             // Request a User
             var userRequestedEvent = new UserRequested();
-            userRequestedEvent.UserName = "Joshua Ramirez";
+            userRequestedEvent.UserName = "Test User";
             UserRequested.Publish(userRequestedEvent);
 
             // Request an Account
             var accountRequestedEvent = new AccountRequested();
-            accountRequestedEvent.AccountName = "Moneypants";
-            accountRequestedEvent.UserId = User.Projections[0].Id;
+            accountRequestedEvent.AccountName = "Test Account";
+            accountRequestedEvent.UserId = User.Projections.Last().Id;
             AccountRequested.Publish(accountRequestedEvent);
-            
+
             // Assert Account Exists
             var accountCount = Account.Projections.Count;
             Assert.Equal(1, accountCount);
@@ -37,8 +38,8 @@ namespace Budget.Application.Tests
             Assert.Equal(1, userCount);
 
             //Assert Account is Linked to User
-            var account = Account.Projections[0];
-            var user = User.Projections[0];
+            var account = Account.Projections.Last();
+            var user = User.Projections.Last();
             Assert.Equal(account.UserId, user.Id);
             Assert.Equal(user.AccountIds[0], account.Id);
         }
